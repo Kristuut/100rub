3655ee74590da979d62298fab568abe42ad3a392
Kristina Sachenko <mytino4ka@mail.ru>
2020-07-27 21:37:31 +0700

    Рассчет бонуса добавлен

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int balance = 100;
         int refill = 1100;
+        long bonus = refill / 100;
         if (refill > 1000){
 
+        } else {
+            bonus = 0;
         }
-        long bonus = refill / balance;
+        
 
-         System.out.println (bonus);
+        System.out.println (bonus);
     }
 }
 
