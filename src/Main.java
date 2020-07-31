4e6b35379fcb6501622c2a1f9ffb640dfcc7d4c0
Kristina Sachenko <mytino4ka@mail.ru>
2020-07-31 23:39:13 +0700

    Перестроен условный оператор if

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill = 1100;
+        int refill = 1200;
         long bonus = refill / 100;
         if (refill > 1000){
+            bonus = refill / 100;
 
         } else {
             bonus = 0;
         }
-        
 
         System.out.println (bonus);
     }
